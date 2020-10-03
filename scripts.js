8d58ea713cd7eeb8401e53956ccc550d1779ef54
arxidamos <dimos33@hotmail.com>
2020-10-03 21:34:34 +0300

    Redesigned functionality to allow chained calculations

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ multiply = (a, b) => {
 
 divide = (a, b) => {
     if (b == 0) {
-        return "Don't divide to zero, douche!"
+        return "Nope!"
     } else {
         return (a / b);
     }
@@ -33,96 +33,148 @@ operate = (operator, a, b) => {
 
 let displayNum = '0';
 let operandOne = operandTwo = null;
-let symbolOne = symbolTwo = '';
+let symbolOne = symbolTwo = null;
+let result = null;
 
-updateDisplay = (value) => {
-    const display = document.querySelector('.item-display');
-    
-
-    // Strip possible starting zero
-    if (display.textContent === '0' || display.textContent == 0) {
-        display.textContent = "";
-    }
-    display.textContent += value.replace(/\r?\n| |\r/g, "");
-    
+updateDisplay = () => {
+    const display = document.querySelector('.item-display'); 
+    display.textContent = displayNum;
+    // Show first 9 digits, calc screen can't fit in more
     if(display.textContent.length > 9) {
         display.textContent = display.textContent.substring(0, 9);
-    }    
-
-    // If AC pressed, clear the display
-    if (value === 'clear') {
-        display.textContent = '0';
     }
-    // Store the current display in the global variable
-    displayNum = display.textContent;
 }
+// updateDisplay();
 
 const buttons = document.querySelectorAll('button');
 buttonClick = () => {
-
     for (let i=0; i<buttons.length; i++) {
         buttons[i].addEventListener('click', (e) => {
 
+            // On number press
             if (buttons[i].classList == "item-number") {
-                // Typing 1st operand
-                if (operandTwo === null) {
-                    updateDisplay(buttons[i].textContent);
-                }
-                // Got 1st operand, pressed a symbol, going for 2nd
-                else if (operandTwo === 0) {
-                    updateDisplay("clear");
-                    updateDisplay(buttons[i].textContent);
-                    operandTwo = Number(displayNum);
-                }
-                // Typing 2nd operand
-                else {
-                    updateDisplay(buttons[i].textContent);
-                }
-
-                // console.log(`here printing...`, buttons[i].textContent);
-            } else if (buttons[i].classList == "item-symbol") {
-
-                // First symbol (no 2nd operand yet)
-                if (symbolOne === '') {
-                    operandOne = Number(displayNum);
-                    operandTwo = 0;
-                    symbolOne = buttons[i].textContent.replace(/\r?\n| |\r/g, "");
-                } 
-                // Second symbol (we need to calculate first two operands)
-                else {
-                    symbolTwo = buttons[i].textContent.replace(/\r?\n| |\r/g, "");
-                    operandTwo = Number(displayNum);
-                    console.log(`operandOne is ${operandOne}`);
-                    console.log(`symbol is ${operandOne}`);
-                    console.log(`operandTwo is ${operandTwo}`);
-                    console.log(`symbol one is ${symbolOne}`);
-                    
-                    // Update display with result
-                    updateDisplay("clear");
-                    displayNum = (operate(symbolOne, operandOne, operandTwo).toString());
-                    updateDisplay(displayNum);
-                    console.log(operate(symbolOne, operandOne, operandTwo));
-
-                    // Get ready for next calculation
-                    symbolOne = '';
-                    symbolTwo = '';
-                    operandOne = displayNum;
-                    operandTwo = 'null';
-
-                }
-
-
-                // console.log(buttons[i].innerHTML);
-            } else if (buttons[i].id == "clear") {
-                updateDisplay('clear');
-                displayNum = '0';
-                operandOne = operandTwo = null;
-                symbolOne = symbolTwo = '';
-                console.log("marika");
+                inputOperand(buttons[i].value);
+                updateDisplay();
+            }
+            // On symbol press
+            else if (buttons[i].classList == "item-symbol") {
+                inputSymbol(buttons[i].value);
+            }
+            else if (buttons[i].id == "clear") {
+                inputAC();
+                updateDisplay();
+            }
+            else if (buttons[i].classList == "item-equals") {
+                inputEquals();
+                updateDisplay();
             }
 
         });
     }
 }
+
+inputOperand = (value) => {
+    // Define operand 1
+    if (symbolOne === null) {
+        // First entry for operand 1 (display is 0)
+        if (displayNum === 0 || displayNum === '0') {
+            displayNum = value;
+            console.log(`Inserting nums - First entry for operand 1`);
+            // operandOne = displayNum;
+        }
+        // First entry for operand 1 (display not 0 but we are after a calculation)
+        else if (displayNum === operandOne) {
+            displayNum = value;
+            console.log(`Inserting nums - First entry for operand 1 after a calculation`);            
+        }
+        // Continue entries for operand 1 (display not 0)
+        else {
+            displayNum += value;
+            console.log(`Inserting nums - Continuing entries for operand 1`);
+        }
+    }
+    // Define operand 2
+    else {
+        // First entry for operand 2
+        if (displayNum === operandOne) {
+            displayNum = value;
+            console.log(`Inserting nums - First entry for operand 2`);
+        }
+        // Continue entries for operand 2
+        else {
+            displayNum += value;
+            console.log(`Inserting nums - Continuing entries for operand 2`);
+        }
+    }
+}
+
+inputAC = () => {
+    displayNum = '0';
+    operandOne = operandTwo = null;
+    symbolOne = symbolTwo = null;
+    result = null;
+}
+
+inputEquals = () => {
+    if (symbolOne === null) {
+        displayNum = displayNum;
+    }
+    else if (symbolTwo !== null) {
+        operandTwo = displayNum;
+        result = operate(symbolOne, Number(operandOne), Number(operandTwo));
+        displayNum = result;
+        operandOne = displayNum;
+        operandTwo = null;
+        symbolOne = symbolTwo = null;
+        result = null;
+    }
+    else {
+        operandTwo = displayNum;
+        result = operate(symbolOne, Number(operandOne), Number(operandTwo));
+        displayNum = result;
+        operandOne = displayNum;
+        operandTwo = null;
+        symbolOne = symbolTwo = null;
+        result = null;
+    }
+
+}
+
+inputSymbol = (symbol) => {
+
+    // First symbol, first time ever
+    if (symbolOne === null && symbolTwo === null) {
+        operandOne = displayNum;
+        console.log("SYMBOL: Our first operand is finished. It is ", operandOne);
+        symbolOne = symbol;
+        console.log("SYMBOL: Our first symbol is ", symbolOne);
+    }
+    // Second symbol, gotta compute now
+    else if (symbolOne !== null && symbolTwo === null) {
+        symbolTwo = symbol;
+        operandTwo = displayNum;
+        console.log("SYMBOL: Our second symbol is ", symbolOne);
+        result = operate(symbolOne, Number(operandOne), Number(operandTwo));
+        console.log(`Latest result is: ${result}`);
+
+        displayNum = Number(result);
+        operandOne = displayNum;
+        result = null;
+        updateDisplay();
+    }
+    // We' ve done at least one computation, move on
+    else if (symbolOne !== null && symbolTwo !== null) {
+        symbolOne = symbolTwo;
+        symbolTwo = symbol;
+        operandTwo = displayNum;
+        result = operate(symbolOne, Number(operandOne), Number(operandTwo));
+        displayNum = Number(result);
+        updateDisplay();
+        operandOne = displayNum;
+        result = null;
+    }
+    return;
+}
+
 
 buttonClick();
